ce46508ee656b2691097aec88a4ecf48bac46e9d
Wolfnebulae <legocrazy00@gmail.com>
2024-03-22 13:40:07 -0600

    Add project files.

---
+++ Mission11_Taylor/Models/EFBookRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Mission11_Taylor.Models
+{
+    public class EFBookRepository : IBookRepository
+    {
+        private BookstoreContext _repo;
+
+        public EFBookRepository(BookstoreContext temp) 
+        {
+            _repo = temp;
+        }
+
+        public IQueryable<Book> Books => _repo.Books;
+    }
+}
---
+++ Mission11_Taylor/Models/IBookRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Mission11_Taylor.Models
+{
+    public interface IBookRepository
+    {
+        public IQueryable<Book> Books { get; }
+    }
+}
